630a80bbd3989278f1d104e6935c89a1ed0702f1
Darya Matskevich <darramatskevich@gmail.com>
2024-09-30 10:02:29 +0300

    fruits

---
+++ lesson-03/03.js
@@ -0,0 +1,3 @@
+const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
+const selectedFruits = fruits.slice(1, 8);
+console.log(selectedFruits);
