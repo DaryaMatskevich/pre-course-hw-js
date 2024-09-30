298302b609986577a320489d9c3fbb64e0494465
Darya Matskevich <darramatskevich@gmail.com>
2024-09-30 09:23:24 +0300

    words

---
+++ lesson-03/02.js
@@ -0,0 +1,8 @@
+​const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+const searchWord = "апельсин";
+if (words.indexOf(searchWord) !== -1) {
+    console.log("Ура! нашел");
+}
+else { 
+    console.log("Придется поискать в другом магазине...")
+}
